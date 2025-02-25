3821dceee949c53aa5d0a0f585f8618e6a8788f6
ballmrk <143125553+ballmrk@users.noreply.github.com>
2025-02-25 17:44:40 -0600

    debugging

--- frontend/app/listing/[id]/page.tsx
+++ frontend/app/listing/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useState, useEffect } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
