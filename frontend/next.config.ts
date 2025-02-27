a819f1500e31d53b394f906d916d8640a1965b1f
ballmrk <143125553+ballmrk@users.noreply.github.com>
2025-02-27 09:56:26 -0600

    update for image deprecation

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,14 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["mls-grid-photos-scott-county.s3.us-east-2.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mls-grid-photos-scott-county.s3.us-east-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
   // other configuration options...
 };
